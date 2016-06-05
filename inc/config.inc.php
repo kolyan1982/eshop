6165619343a2217caff007c0b38dc9fa5a240660
nikolay <kolysh23@yandex.ru>
2016-06-05 18:30:18 +0300

    add configuration

--- inc/config.inc.php
+++ inc/config.inc.php
@@ -1,1 +1,20 @@
 <?php
+header("Content-Type: text/html; charset=utf-8");
+
+const DB_HOST = 'localhost';
+const DB_LOGIN = 'root';
+const DB_PASSWORD = '';
+const DB_NAME = 'eshop';
+const ORDERS_LOG = 'orders.log';
+
+//Для хранения корзины пользователя
+$basket = [];
+//Хранение количество товаров в корзине пользователя
+$count = 0;
+//Устанавливаем соединение с БД MySQL
+$link = mysqli_connect(DB_HOST, DB_LOGIN, DB_PASSWORD, DB_NAME);
+
+if (mysqli_connect_errno()) {
+    echo "Не удалось установить соединение";
+    exit;
+}
