c818387ab8163900082de7a8587928cbcf42e13c
nikolay <kolysh23@yandex.ru>
2016-06-06 11:58:51 +0300

    addItemToCatalog

--- inc/lib.inc.php
+++ inc/lib.inc.php
@@ -1,1 +1,18 @@
 <?php
+
+//Функция сохраняет новый товар
+function addItemToCatalog($title, $author, $pubyear, $price){
+    global $link;
+
+    $sql = "INSERT INTO catalog (title, author, pubyear, price) 
+                   VALUES (?, ?, ?, ?)";
+
+    if (!$stmp = mysqli_prepare($link, $sql)) {
+        return false;
+    }
+
+    mysqli_stmt_bind_param ($stmt, "ssii", $title, $author, $pubyear, $price);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+    return true;
+}
